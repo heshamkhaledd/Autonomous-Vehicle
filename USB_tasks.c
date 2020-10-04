95ee0ad3d6aadad15c67d201864df172ccb18157
Y-Galal <yousifg177@gmail.com>
2020-10-04 17:33:42 +0200

    Creating USB_branch

--- USB_tasks.c
+++ USB_tasks.c
@@ -18,13 +18,7 @@ static void * vpCDCDevice = NULL ;
 
 
 
-//*****************************************************************************
-//
-// Flags used to pass commands from interrupt context to the main loop.
-//
-//*****************************************************************************
-#define COMMAND_PACKET_RECEIVED 0x00000001
-#define COMMAND_STATUS_UPDATE   0x00000002
+
 
 
 volatile uint32_t g_ui32Flags = 0;
