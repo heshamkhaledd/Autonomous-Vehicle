1889c272e1d37e1ffd34256a28f0933280561294
Hesham Khaled <heshamkhaled13@gmail.com>
2020-10-06 20:04:48 +0200

    fixed decoding errors and edited its file

--- State_Decode.c
+++ State_Decode.c
@@ -9,36 +9,87 @@
  ******************************************************************************/
 #include "State_Decode.h"
 
+uint8_t letter_index = 0;
+
 
 /******************************************************************************
  *
- * Function Name: f_Orientation_Decoding
+ * Function Name: f_ASCIItoDecimal
  *
- * Description: Responsible for converting USB data from ASCII to Decimal
+ * Description: Responsible for converting ASCII data to Decimal data
  *
- * Arguments:   unsigned long USB_Received_Angle , float New_Orientation_Angle  , long point_flag
- * Return:      float New_Orientation_Angle
+ * Arguments:   uint8_t *Data
+ * Return:      float number
  *
  *****************************************************************************/
-static float  f_Orientation_Decoding (unsigned long USB_Received_Angle , float New_Orientation_Angle  , long point_flag)
+static float f_ASCIItoDecimal (uint8_t * Data)
 {
-    static long counter = 0 ;
-    if ( point_flag == 0)
+    uint8_t negative_flag = 0;
+    uint8_t point_index = 0;
+    uint8_t multiplier = 10;
+    float number = 0.0;
+    
+    /* Loop on the given array to determine the the decimal point location, letter location, and check if the number is negative */
+    while (Data[letter_index] != '\0')
     {
-        counter = 0 ;
-        New_Orientation_Angle = New_Orientation_Angle * 10 ;
-        USB_Received_Angle = USB_Received_Angle - Numbers_Ascii_Base ; /* convert number from ASCII to Decimal */
-        New_Orientation_Angle = New_Orientation_Angle + (float)USB_Received_Angle ;
-        return New_Orientation_Angle ;
+        switch (Data[letter_index])
+        {
+            case '.':
+                point_index = letter_index++;
+                break;
+
+            case '-':
+                negative_flag = 1;
+                letter_index++;
+                break;
+
+            default:
+                letter_index++;
+                break;
+        }                  
     }
+    
+    /* deceremt the letter_index by 1 because it was pointing at the '\0' */
+    letter_index --;
+
+    /* If the data was a whole number with no decimal points, we can iterate to the end of the numbers*/
+    if (point_index == 0)
+    {
+        uint8_t i;
+        for (i = negative_flag ; i<letter_index ; i++)
+        {
+        number *= 10;
+        Data[i] = Data[i] -  Numbers_Ascii_Base;
+        number = number + Data[i];
+        }
+    }
+    /* If the data had decimals, we must process the number on two steps. First the whole numbers, then the decimals */
     else
     {
-        counter ++ ;
-        USB_Received_Angle = USB_Received_Angle - Numbers_Ascii_Base ; /* convert number from ASCII to Decimal */
-        New_Orientation_Angle = New_Orientation_Angle + (float)(USB_Received_Angle/ ( 10 * counter )) ;
-        return New_Orientation_Angle ;
+        uint8_t i;
+        for (i = negative_flag ; i<point_index ; i++)
+        {
+        number *= 10;
+        Data[i] = Data[i] -  Numbers_Ascii_Base;
+        number = number + Data[i];
+        }
+        
+        i +=1;
+
+        while (i<letter_index)
+        {
+            Data[i] = Data[i] - Numbers_Ascii_Base;
+            number = number + (float)(Data[i]) / (float)(multiplier);
+            multiplier *= 10;
+            i++;
+        }
     }
 
+    /* If the number was negative, we must multiply with -1 */
+    if (negative_flag == 1)
+        number = number * -1;
+
+    return number;
 }
 
 
@@ -46,61 +97,61 @@ static float  f_Orientation_Decoding (unsigned long USB_Received_Angle , float N
  *
  * Function Name: State_Decoding
  *
- * Description: USB Task that's responsible for receiving the Orientation data
- *              decode it, and push it to the appropriate queue.
+ * Description: Responsible for pushing the Orientation/Throttle Data to the
+ *              appropriate queue according to the module who called it.
  *
- * Arguments:   char USB_Received_Char
+ *
+ * Arguments:   uint8_t * Data, uint8_t Module
  * Return:      void
  *
  *****************************************************************************/
-void State_Decoding (char USB_Received_Char)
+void State_Decoding (uint8_t * Data, uint8_t Module)
 {
-    static  float f_Orientation = 0; /* Getting Final Angle in here */
-    static  long i32_Sign = 1;
-    static long point_flag = 0 ;
-    float f_Desired_Orientation ;
 
-    if(((USB_Received_Char) > (Numbers_Ascii_Base + Numbers_Ascii_Offset)) || ((USB_Received_Char) < (Numbers_Ascii_Base)))
+    float number = 0.0;
+
+    uint8_t error_flag = 1;
+    
+    number = f_ASCIItoDecimal (Data);
+
+    switch (Module)
     {
-        /*  which motor we will modify its current angle or if a negative angle */
-        switch (USB_Received_Char){
-        /* O for Orientation */
-        case 'o' :
-        case 'O' :
-            f_Desired_Orientation = f_Orientation * i32_Sign ;
-            point_flag = 0;
-            i32_Sign=1;
-            f_Orientation =0 ;
-            //Queue steering steps
-            xQueueOverwrite(Queue_Desired_Orientation,
-                            &f_Desired_Orientation);
+        case USB_MODULE:
+                    switch(Data[letter_index])
+                    {
+                        case 'O':
+                        case 'o':
+                                xQueueOverwrite(Queue_Desired_Orientation,
+                                                &number);
+                                break;
+                        case 'T':
+                        case 't':
+                                /* Throttle Code to go here */
+                                break;
+                        default:
+                                error_flag = 1;
+                    }
 
-            //Queue send Desired Orientation
-            break;
-            /* S for speed */
-        case 's' :
-        case 'S' :
-
-            break ;
-
-        case '.' :
-            point_flag = 1;
-            break ;
-            /* - for negative Orientation */
-        case '-' :
-            i32_Sign=-1;
-            f_Orientation = 0 ;
-            break ;
-
-            /* unexpected Received data Case */
-        default :
-            i32_Sign=1;
-            f_Orientation = 0 ;
-        }
+        case UART_MODULE:
+                    switch(Data[letter_index])
+                    {
+                        case 'O':
+                        case 'o':
+                                xQueueOverwrite(Queue_Current_Orientation,
+                                                &number);
+                                break;
+                        case 'T':
+                        case 't':
+                                /* Throttle Code to go here */
+                                break;
+                        default:
+                                error_flag = 1;
+                    }
     }
-    else
+
+    if (error_flag == 1)
     {
-        /* calculating desired angle */
-        f_Orientation = f_Orientation_Decoding ( USB_Received_Char , f_Orientation , point_flag) ;
+        /* Code to Handle Frame Error */
     }
+    letter_index = 0;
 }
--- UART_TASK.c
+++ UART_TASK.c
@@ -8,44 +8,11 @@
  *
  ******************************************************************************/
 #include "UART_TASK.h"
-
+#include "State_Decode.h"
 
 
 /* Declare Semaphore Handle for UART task */
 SemaphoreHandle_t Sem_UARTReceive;
-
-
-/******************************************************************************
- *
- * Function Name: f_UART_Decoding
- *
- * Description: Responsible for converting UART Feedback data from ASCII to Decimal
- *
- * Arguments:   char UART_Received_Char , float Decoded_Data  , long point_flag
- * Return:      float Decoded_Data
- *
- *****************************************************************************/
-static float f_UART_Decoding (char UART_Received_Char , float Decoded_Data  , long point_flag)
-{
-    static long counter = 0 ;
-    if ( point_flag == 0)
-    {
-        counter = 0 ;
-        Decoded_Data = Decoded_Data * 10 ;
-        Decoded_Data = UART_Received_Char - Numbers_Ascii_Base ; /* convert number from ASCII to Decimal */
-        Decoded_Data = Decoded_Data + (float)UART_Received_Char ;
-        return Decoded_Data ;
-    }
-    else
-    {
-        counter ++ ;
-        Decoded_Data = Decoded_Data - Numbers_Ascii_Base ; /* convert number from ASCII to Decimal */
-        Decoded_Data = Decoded_Data + (float)(Decoded_Data/ ( 10 * counter )) ;
-        return Decoded_Data ;
-    }
-
-}
-
 
 /******************************************************************************
  *
@@ -75,8 +42,8 @@ void vInit_UART(void)
  *
  * Function Name: vInit_UART
  *
- * Description: UART Task that's responsible for receiving the Orientation data
- *              decode it, and push it to the appropriate queue.
+ * Description: UART Task that's responsible for receiving the Orientation Data
+ *              and store each character in a software buffer (UART_Received_Frame)
  *
  * Arguments:   void *pvParameters
  * Return:      void
@@ -84,95 +51,31 @@ void vInit_UART(void)
  *****************************************************************************/
 void vTask_UART(void *pvParameters)
 {
-    char UART_Received_Char = 0 ;
-    signed long Received_Data = 0;
-    float f_Orientation = 0 ;
-    static long point_flag  = 0 ;
-    static signed long sign_flag = 1 ;
 
-    while(1){
+    uint8_t UART_Received_Frame[arr_size];
+
+    static uint8_t index = 0;
+
+    while(1)
+    {
         xSemaphoreTake(Sem_UARTReceive,portMAX_DELAY);
-        while (UARTCharsAvail(UART1_BASE)){ /* Check if there received char in the buffer or not */
 
-            UART_Received_Char = UARTCharGet(UART1_BASE); /* get Received char and decode it */
+        /* Check if there received char in the buffer or not */
+        while (UARTCharsAvail(UART1_BASE))
+        {
+            UART_Received_Frame[index] = UARTCharGet(UART1_BASE); /* Get the received char */
 
-            if(((UART_Received_Char) > (Numbers_Ascii_Base + Numbers_Ascii_Offset)) || ((UART_Received_Char) < (Numbers_Ascii_Base)))
+            /* If the received character is a letter. The frame is completed, we must start decoding it */
+            if (UART_Received_Frame[index] == 'o' || UART_Received_Frame[index] == 'O')
             {
-                /*  which motor we will modify its current angle or if a negative angle */
-                switch (UART_Received_Char){
-                /* O for Orientation */
-                case 'o' :
-                case 'O' :
-                    f_Orientation = (float) Received_Data * sign_flag ;
-                    //Queue send Desired Orientation
-                    xQueueOverwrite(Queue_Current_Orientation, &f_Orientation);
-                    /* back to default to receive new data */
-                    Received_Data = 0;
-                    sign_flag = 1 ;
-                    point_flag = 0 ;
-                    break;
-
-                case 'k' :
-                case 'K' :
-                    /* back to default to receive new data */
-                                       Received_Data = 0;
-                                       sign_flag = 1 ;
-                                       point_flag = 0 ;
-                    break;
-                case 'l' :
-                case 'L' :
-                    /* back to default to receive new data */
-                                       Received_Data = 0;
-                                       sign_flag = 1 ;
-                                       point_flag = 0 ;
-                    break;
-                case 'm' :
-                case 'M' :
-                    /* back to default to receive new data */
-                                       Received_Data = 0;
-                                       sign_flag = 1 ;
-                                       point_flag = 0 ;
-                    break;
-                case 'n' :
-                case 'N' :
-                    /* back to default to receive new data */
-                                       Received_Data = 0;
-                                       sign_flag = 1 ;
-                                       point_flag = 0 ;
-                    break;
-                case 'p' :
-                case 'P' :
-                    /* back to default to receive new data */
-                                       Received_Data = 0;
-                                       sign_flag = 1 ;
-                                       point_flag = 0 ;
-                    break;
-
-                case 'q' :
-                case 'Q' :
-                    /* back to default to receive new data */
-                                       Received_Data = 0;
-                                       sign_flag = 1 ;
-                                       point_flag = 0 ;
-                    break;
-
-                case '.' : /* handling floating numbers */
-                    point_flag = 1 ;
-                    break ;
-
-                case '-' : /* handling negative numbers */
-                    sign_flag = -1 ;
-                    /* unexpected Received data Case */
-                default :
-                   /* handling unexpected data */
-                    break;
-                }
+                UART_Received_Frame[++index] = '\0';
+                break;
             }
-            else
-            {
-                /* if the received char is number, update received data */
-                Received_Data =  f_UART_Decoding (UART_Received_Char ,  Received_Data  , point_flag);
-            }
+
+            index++;
         }
+
+        State_Decoding (UART_Received_Frame, UART_MODULE);
+        index = 0;
     }
 }
